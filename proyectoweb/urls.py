68cd4954203110d1286084bcb7a4e92318ca92ea
Velazquezadrian <adrianvelazquez528@outlook.com>
2021-01-15 20:54:18 -0300

    15/01

--- proyectoweb/urls.py
+++ proyectoweb/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('servicios/', include("servicios.urls")),
     path('blog/', include("blog.urls")),
+    path('contacto/', include("contacto.urls")),
     path('', include('proyectowebapp.urls')),
 ]
--- proyectowebapp/views.py
+++ proyectowebapp/views.py
@@ -11,6 +11,3 @@ def tienda(request):
 
     return render(request, "proyectowebapp/tienda.html")
 
-def contacto(request):
-
-    return render(request, "proyectowebapp/contacto.html")    
